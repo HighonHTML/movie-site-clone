c122b6b4204e3b1e83641b41495f21ed07ae7973
bharath197 <bharath.anbuselvan7@gmail.com>
2024-04-11 00:28:21 +0530

    movie site

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating.jsx";
+import App from "./App.jsx";
+import './index.css'
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  const HandleExternalRating = (movieRating) => setMovieRating(movieRating);
+  return (
+    <div>
+      <StarRating
+        HandleExternalRating={HandleExternalRating}
+        color="orange"
+      ></StarRating>
+      <p>this movie has {movieRating} stars</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
